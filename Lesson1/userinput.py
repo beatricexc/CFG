ae13fe7164674b8d150fdd1ed8d54ca8a9f85e35
beatricx <72341578+beatricexc@users.noreply.github.com>
2022-10-05 21:30:53 +0100

    Create userinput.py

---
+++ Lesson1/userinput.py
@@ -0,0 +1,12 @@
+# Create a program that asks for 2 numbers.
+# Create a variable that adds the two numbers and one that multiplies the two numbers.
+# Print the results by using the f 
+
+number_1 = int(input("Type in any number: "))
+number_2 = int(input("Type in your second random number: "))
+
+addition = number_1 + number_2
+multiplication = number_1 * number_2
+
+print(f"The result of addition is {addition}")
+print(f"The result of multiplication is {multiplication}")
