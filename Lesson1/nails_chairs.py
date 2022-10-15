b07265ffa7b9e0ca7e970200ef047730a8380d8c
beatricx <72341578+beatricexc@users.noreply.github.com>
2022-10-15 21:53:09 +0100

    Create nails_chairs.py

---
+++ Lesson1/nails_chairs.py
@@ -0,0 +1,17 @@
+# Q1 I am building some very high quality chairs and need exactly four nails for each chair.
+# I have written a program to calculate how many nails I need to buy to build these charis
+
+chairs = '15'
+nails = 4
+
+total_nails = chairs* nails
+
+message = '{} nails'.format(total_nails)
+
+print('You need to buy {}'.format(message))
+
+# When I run the program it tells me that I need to buy 15151515 nails. There seems like a lot of nails
+#Is my program calculating the total number correctly? What is the problem?
+      
+# Solution : The problem is that chairs is a string instead of an int so we need to eliminate the single quotation marks from '15'
+      
